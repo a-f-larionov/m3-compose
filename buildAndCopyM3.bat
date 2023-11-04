c15e3b274a5bc5afcf9d95bbe6e03381e2a1f39b
Larionov Alexandr <a.f.larionov@gmail.com>
2023-11-04 16:22:24 +0300

    - update m3

--- buildAndCopyM3.bat
+++ buildAndCopyM3.bat
@@ -1,4 +1,4 @@
-@echo off
+rem @echo off
 
 CALL :buildAndCopyToServer m3-common-service
 CALL :buildAndCopyToServer m3-users-service
@@ -47,8 +47,8 @@ SETLOCAL
 SET PROJECT_FOLDER=%1
 
 echo "start copy to server"
-ssh root@prod-server-2.ru "mkdir /var/job/m3/%PROJECT_FOLDER%/build  2> null
-ssh root@prod-server-2.ru "mkdir /var/job/m3/%PROJECT_FOLDER%/build/libs  2> null
+ssh root@prod-server-2.ru "mkdir /var/job/m3/%PROJECT_FOLDER%/build
+ssh root@prod-server-2.ru "mkdir /var/job/m3/%PROJECT_FOLDER%/build/libs
 cd ..
 scp -pr ./%PROJECT_FOLDER%/build/libs/%PROJECT_FOLDER%-0.0.1-SNAPSHOT.jar root@prod-server-2.ru:/var/job/m3/%PROJECT_FOLDER%/build/libs/%PROJECT_FOLDER%-0.0.1-SNAPSHOT.jar
 cd m3-compose
