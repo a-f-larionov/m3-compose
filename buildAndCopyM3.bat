448747eedd80e3278db20636dc3ea04066db8875
Larionov Alexandr <a.f.larionov@gmail.com>
2023-12-04 17:51:34 +0300

    - update build and copy batch file

--- buildAndCopyM3.bat
+++ buildAndCopyM3.bat
@@ -1,73 +1,27 @@
 rem @echo off
 
-CALL :publishToLocalMaven m3-lib
+cd ..
+call gradle publishToMavenLocal build -x test
 
-CALL :buildAndCopyToServer m3-common-service
-CALL :buildAndCopyToServer m3-users-service
-CALL :buildAndCopyToServer m3-game-service
+call :copyToServer m3-common-service
+call :copyToServer m3-users-service
+call :copyToServer m3-game-service
 
-REM CALL :dockerRestart
-
-docker compose up -d --build
+cd m3-compose
 
 goto :eof
 
-:dockerRestart
-SETLOCAL
-    ssh root@prod-server-2.ru "cd /var/job/m3/m3-compose && docker compose up -d --build"
-ENDLOCAL
-EXIT /B
-
-:buildAndCopyToServer
-SETLOCAL
-SET PROJECT_FOLDER=%1
-
-call :build %PROJECT_FOLDER%
-call :copyToServer %PROJECT_FOLDER%
-
-ENDLOCAL
-EXIT /B
-
-:build
-SETLOCAL
-SET PROJECT_FOLDER=%1
-
-echo build %PROJECT_FOLDER%
-cd ..
-cd %PROJECT_FOLDER%
-
-call gradle build --warning-mode all -x test
-
-cd ..
-cd m3-compose
-
-ENDLOCAL
-EXIT /B
+REM    ssh root@prod-server-2.ru "cd /var/job/m3/m3-compose && docker compose up -d --build"
 
 :copyToServer
 SETLOCAL
 SET PROJECT_FOLDER=%1
 
 echo "start copy to server"
-ssh root@prod-server-2.ru "mkdir /var/job/m3/%PROJECT_FOLDER%/build
-ssh root@prod-server-2.ru "mkdir /var/job/m3/%PROJECT_FOLDER%/build/libs
-cd ..
+@ssh root@prod-server-2.ru "mkdir /var/job/m3/%PROJECT_FOLDER%/build
+@ssh root@prod-server-2.ru "mkdir /var/job/m3/%PROJECT_FOLDER%/build/libs
+
 scp -pr ./%PROJECT_FOLDER%/build/libs/%PROJECT_FOLDER%-0.0.1-SNAPSHOT.jar root@prod-server-2.ru:/var/job/m3/%PROJECT_FOLDER%/build/libs/%PROJECT_FOLDER%-0.0.1-SNAPSHOT.jar
-cd m3-compose
-
-ENDLOCAL
-EXIT /B
-
-:publishToLocalMaven
-SETLOCAL
-SET PROJECT_FOLDER=%1
-
-echo publishToMavenLocal %PROJECT_FOLDER%
-cd ..
-cd %PROJECT_FOLDER%
-call gradle publishToMavenLocal -x test
-cd ..
-cd m3-compose
 
 ENDLOCAL
 EXIT /B
